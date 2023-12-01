6b6bd3b44970670757258a9004bc84087e9b7083
Ehsan <esihazar@gmail.com>
2023-12-01 18:03:53 -0500

    Created a TypeScript declaration file 'vuex.d.ts' in the root directory to declare the structure of the Vuex module, specifying the Vuex application setup. This declaration provides type information for the Vuex module and enhances TypeScript integration within the project.

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -12,11 +12,48 @@
       </div>
     </nav>
 
+    <form action="" class="ticket-form">
+      <div>
+        <label for="ticketId">Ticket ID:</label>
+        <input type="text" id="ticketId" v-model="ticketId" />
+      </div>
+
+      <div>
+        <label for="ticketName">Ticket Name:</label>
+        <input type="text" id="ticketName" v-model="ticketName" />
+      </div>
+
+      <div>
+        <label for="description">Description:</label>
+        <input type="text" id="description" v-model="description" />
+      </div>
+
+      <div>
+        <label for="isVip">VIP Ticket:</label>
+        <input type="checkbox" id="isVip" v-model="isVip" />
+      </div>
+
+      <div>
+        <label for="count">Ticket Count:</label>
+        <input type="number" id="count" v-model="count" />
+      </div>
+
+      <div>
+        <label for="price">Ticket Price:</label>
+        <input type="number" step="0.01" id="price" v-model="price" />
+      </div>
+    </form>
+
     <!-- Display the content based on the current route -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {};
+import tempData from "../temp-data";
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
